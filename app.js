eb18ec8a968f1ed9069e17f49de2f00bb6a3b382
Rohit <rohitkori897@gmail.com>
2022-06-12 22:17:37 +0530

    Bug fixed in get article time command

--- app.js
+++ app.js
@@ -373,12 +373,23 @@ client.on("message", (msg) => {
           });
       } else {
         if (msgRecievied[2] == "time") {
-          let hr_OnSheet;
-          getArticle((msg.guild.id).toString()).then(response => { hr_OnSheet = response })
-          setTimeout(() => {
-            rule.hour = parseInt(hr_OnSheet.hr);
-            rule.minute = parseInt(hr_OnSheet.min);
-          }, 1300)
+          try {
+            let hr_OnSheet;
+            getArticle((msg.guild.id).toString()).then(response => { hr_OnSheet = response })
+            setTimeout(() => {
+              rule.hour = parseInt(hr_OnSheet.hr);
+              rule.minute = parseInt(hr_OnSheet.min);
+            }, 1300)
+          }
+          catch {
+            console.log("Unable to fetch time at the moment")
+          }
+          // let hr_OnSheet;
+          // getArticle((msg.guild.id).toString()).then(response => { hr_OnSheet = response })
+          // setTimeout(() => {
+          //   rule.hour = parseInt(hr_OnSheet.hr);
+          //   rule.minute = parseInt(hr_OnSheet.min);
+          // }, 1300)
 
           setTimeout(() => {
             var cronExpression = `${rule.minute} ${rule.hour} * * ${startDay}-${endDay}`;
